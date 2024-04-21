926b664e3083193498f428bb8ed2f087a7199eb5
Hanna Lee <hannalee809@gmail.com>
2024-04-21 17:07:47 -0400

    added sample data and documentation for data

---
+++ R/sample_data.R
@@ -0,0 +1,14 @@
+#' Sample Tasks Data
+#'
+#' This is a example of a data frame consisting of various tasks.
+#'
+#' @format ## 'sample_data'
+#' A data frame with 8 rows and 5 columns:
+#' \describe{
+#'   \item{Description}{Description of the task}
+#'   \item{Due_Date}{When the task is due (Month-Day-Year)}
+#'   \item{Priority}{The priority level of the task (High, Medium, Low)}
+#'   \item{Category}{The category the task falls under. It could be for a certain class or personal}
+#'   \item{Completed}{Whether the task is complete or incomplete}
+#' }
+"sample_data"
