69c93646528c765acd16ae667dbaee56226e61d7
Hanna Lee <hannalee809@gmail.com>
2024-04-21 12:52:15 -0400

    added elements to summary function

--- R/delete.R
+++ R/delete.R
@@ -1,6 +1,5 @@
 delete_task <- function(tasks) {
   repeat {
-    # if there are no current tasks
     if (nrow(tasks) == 0) {
       cat("No tasks to delete.\n")
     }
@@ -8,8 +7,7 @@ delete_task <- function(tasks) {
     cat("Current tasks:\n")
     print(tasks)
 
-    description <-
-      readline(prompt = "Enter the description of the task you want to delete (or type 'done'): ")
+    description <- readline(prompt = "Enter the description of the task you want to delete (or type 'done'): ")
 
     if (tolower(description) == "done") {
       break
--- R/summary.R
+++ R/summary.R
@@ -1,12 +1,37 @@
+library(ggplot2)
+summary_tasks <- function(tasks) {
+  cat("Summary of Tasks:\n")
 
-summary <- function(tasks) {
+  # total number of tasks
+  cat("You have a total of", nrow(tasks), "tasks.\n")
 
-  # print summary output for tasks
-  overall
+ # total number of completed and incomplete tasks
+  cat("Number of completed tasks: ", sum(tasks$Completed), "\n")
+  cat("Number of incomplete tasks: ", sum(tasks$Completed == FALSE), "\n")
 
+  # tasks with highest priority
+  # tasks$Priority <- factor(tasks$Priority, levels = c("High", "Medium", "Low"))
+  highest_priority <- tasks %>%
+    filter(Priority == "High")
+  cat("You're highest priority tasks: \n")
+  print(highest_priority)
 
-  # visualization for category
-
+  # tasks with closest due date
+  tasks$Due_Date <- strptime(tasks$Due_Date, "%m-%d-%y")
+  closest_due_date_task <- tasks[order(tasks$Due_Date)[1], ]
+  cat("\nTask with closest due date:\n")
+  print(closest_due_date_task)
 
   # visualization for priority
+  priority_plot <- ggplot(tasks, aes(x = Priority)) +
+    geom_bar(fill = "skyblue", color = "black") +
+    labs(
+      title = "Distribution of Task Priorities",
+      x = "Task Priority",
+      y = "Number of tasks"
+    ) +
+    theme_minimal()
+
+  print(priority_plot)
 }
+
