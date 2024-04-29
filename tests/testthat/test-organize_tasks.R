2b3b28299300ab77be9fbab7a1f866229b350423
reagangonzales <rgonzales@smith.edu>
2024-04-29 09:08:02 -0400

    edited organize function

--- tests/testthat/test-organize_tasks.R
+++ tests/testthat/test-organize_tasks.R
@@ -10,16 +10,27 @@ tasks <- data.frame(
 
 # Testing organize by due date
 testthat::test_that("Organizing tasks by due date works correctly", {
+  # Create a sample data frame of tasks
+  tasks <- data.frame(
+    Description = c("Task 1", "Task 2", "Task 3"),
+    Due_Date = c("2024-04-15", "2024-04-12", "2024-04-20"),
+    Priority = c("High", "Medium", "Low"),
+    Category = c("Work", "Personal", "Work"),
+    Completed = c(FALSE, FALSE, TRUE)
+  )
+
   # Call the organize function to organize tasks by due date
-  organized_tasks <- organize_tasks(tasks)
+  organized_tasks <- organize_tasks(tasks, choice = 1)
+
   # Check if tasks are organized correctly
   testthat::expect_identical(organized_tasks$Due_Date, sort(organized_tasks$Due_Date))
 })
 
+
 # Testing organize by completion status
 testthat::test_that("Organizing tasks by completion status works correctly", {
   # Call the organize function to organize tasks by completion status
-  organized_tasks <- organize_tasks(tasks)
+  organized_tasks <- organize_tasks(tasks, choice = 2)
   # Check if tasks are organized correctly
   testthat::expect_identical(organized_tasks$Completed, rev(sort(organized_tasks$Completed)))
 })
@@ -27,7 +38,7 @@ testthat::test_that("Organizing tasks by completion status works correctly", {
 # Testing organize by priority level
 testthat::test_that("Organizing tasks by priority level works correctly", {
   # Call the organize function to organize by priority level
-  organized_tasks <- organize_tasks(tasks)
+  organized_tasks <- organize_tasks(tasks, choice = 4)
   # Check if tasks are organized correctly
   testthat::expect_identical(organized_tasks$Priority, sort(organized_tasks$Priority))
 })
@@ -35,7 +46,7 @@ testthat::test_that("Organizing tasks by priority level works correctly", {
 # Testing organize by category
 testthat::test_that("Organizing tasks by category works correctly", {
   # Call organize function to organize by priority level
-  organized_tasks <- organize_tasks(tasks)
+  organized_tasks <- organize_tasks(tasks, choice = 3)
   # Check if tasks are organized correctly
   testthat::expect_identical(organized_tasks$Category, sort(organized_tasks$Category))
 })
